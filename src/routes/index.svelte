077dbe3f1035caef2e86d78b503b74be62c71a72
Antoine Richard <antoiner.info@gmail.com>
2022-08-31 22:25:17 +0200

    add style for applied coupon

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -16,7 +16,12 @@
 		editing = true;
 	}
 
+	function validateCoupon(value) {
+		return value.trim()?.length > 4;
+	}
+
 	async function handleSubmit() {
+		if(!validateCoupon(coupon)) return;
 		loading = true;
 		return new Promise((resolve) =>
 			setTimeout(() => {
@@ -29,15 +34,15 @@
 </script>
 
 <div class="card mt-4">
+	<span class="block text-gray-700 text-sm font-bold mb-2">Code Promo</span>
 	{#if editing}
 		<form id="" action="#" on:submit|preventDefault={handleSubmit}>
-			<label for="coupon-code" class="block text-gray-700 text-sm font-bold mb-2">Code Promo</label>
 			<div class="flex">
 				<input
 					type="text"
 					id="coupon-code"
 					placeholder="000000"
-					bind:value={coupon}
+					on:input={(event) => (coupon = event.target.value)}
 					class="textinput mr-2"
 					disabled={loading}
 				/>
@@ -52,8 +57,16 @@
 	{:else if error}
 		<button class="btn btn-secondary" on:click={handleRetry}>Retry</button>
 	{:else}
-		<span>{coupon}</span>
-		<button class="btn btn-secondary" on:click={handleDelete}>Delete</button>
+		<div class="flex justify-between items-center">
+			<span
+				aria-label="supprimer"
+				class="text-xs inline-flex items-center font-bold leading-sm uppercase px-3 py-1 rounded-full bg-white text-gray-700 border"
+			>
+				{coupon}
+				<svg class="ml-2" style="height: 14px;" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M438.6 105.4C451.1 117.9 451.1 138.1 438.6 150.6L182.6 406.6C170.1 419.1 149.9 419.1 137.4 406.6L9.372 278.6C-3.124 266.1-3.124 245.9 9.372 233.4C21.87 220.9 42.13 220.9 54.63 233.4L159.1 338.7L393.4 105.4C405.9 92.88 426.1 92.88 438.6 105.4H438.6z"/></svg>
+			</span>
+			<button class="btn btn-error" on:click={handleDelete}>Supprimer</button>
+		</div>
 	{/if}
 
 	<div class="mt-4">
